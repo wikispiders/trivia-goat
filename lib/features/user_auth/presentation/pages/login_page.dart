7f59f3e682e167d7fafdc1733bd4d1956237bc52
mateocapon <mcapon@fi.uba.ar>
2023-11-06 20:04:42 -0300

    login

--- lib/features/user_auth/presentation/pages/login_page.dart
+++ lib/features/user_auth/presentation/pages/login_page.dart
@@ -1,10 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:frases_argentinas/features/user_auth/presentation/firebase_auth/firebase_auth_services.dart';
 import 'package:frases_argentinas/features/user_auth/presentation/pages/home_page.dart';
 import 'package:frases_argentinas/features/user_auth/presentation/pages/signup_page.dart';
 import 'package:frases_argentinas/features/user_auth/presentation/widgets/form_container_widget.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuthService _auth = FirebaseAuthService();
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -25,17 +43,17 @@ class LoginPage extends StatelessWidget {
               FormContainerWidget(
                 hintText: "Email",
                 isPasswordField: false,
+                controller: _emailController
               ),
               SizedBox(height: 10,),
               FormContainerWidget(
                 hintText: "Password",
                 isPasswordField: true,
+                controller: _passwordController
               ),
               SizedBox(height: 30,),
               GestureDetector(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-                },
+                onTap: _login,
                 child: Container(
                   width: double.infinity,
                   height: 45,
@@ -68,5 +86,19 @@ class LoginPage extends StatelessWidget {
         ),
       )
     );
+  }
+
+  void _login() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    User? user = await _auth.signInWithEmailAndPassword(email, password);
+
+    if (user != null) {
+      print("User successfully login");
+      Navigator.pushNamed(context, "/home");
+    } else {
+      print("Error occurred login");
+    }
   }
 }
