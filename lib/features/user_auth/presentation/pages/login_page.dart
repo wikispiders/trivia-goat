e7de6d91a39ecbc430833c6f6cef3755e4bad4ba
mateocapon <mcapon@fi.uba.ar>
2023-11-06 16:57:17 -0300

    diseno login arrancado

--- lib/features/user_auth/presentation/pages/login_page.dart
+++ lib/features/user_auth/presentation/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frases_argentinas/features/user_auth/presentation/widgets/form_container_widget.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,13 +8,44 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
+        title: const Text("Login"),
       ),
-      body: Column(
-        children: [
-          Text("Login", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold))
-        ],
-      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Login", style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 30,
+              ),
+              FormContainerWidget(
+                hintText: "Email",
+                isPasswordField: false,
+              ),
+              SizedBox(height: 10,),
+              FormContainerWidget(
+                hintText: "Password",
+                isPasswordField: true,
+              ),
+              SizedBox(height: 30,),
+              Container(
+                width: double.infinity,
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                )
+              ),
+
+            ],
+          ),
+        ),
+      )
     );
   }
 }
