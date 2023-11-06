1c135f69d1623bae30fb44365a01c9c80fdbc515
mateocapon <mcapon@fi.uba.ar>
2023-11-06 16:18:03 -0300

    arrancando login

---
+++ lib/features/user_auth/presentation/pages/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Column(
+        children: [
+          Text("Login", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold))
+        ],
+      ),
+    );
+  }
+}
