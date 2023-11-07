dfa7344fa2b02b4e6e4e2714ebfdb8887c8caf3d
milisanchez00 <mvsanchez@fi.uba.ar>
2023-11-07 18:13:28 -0300

    preparada la pag para hacer las instrucciones y el start buttom

---
+++ lib/features/app/instructions/instructions_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class InstructionsPage extends StatefulWidget {
+  const InstructionsPage({super.key});
+
+  @override
+  State<InstructionsPage> createState() => _InstructionsPageState();
+}
+
+class _InstructionsPageState extends State<InstructionsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:frases_argentinas/features/app/instructions/instructions_page.dart';
 import 'package:frases_argentinas/features/app/splash_screen/splash_screen.dart';
 import 'package:frases_argentinas/features/user_auth/presentation/pages/home_page.dart';
 import 'package:frases_argentinas/features/user_auth/presentation/pages/login_page.dart';
@@ -38,6 +39,7 @@ class MyApp extends StatelessWidget {
         '/login':(context) => LoginPage(),
         '/signUp':(context) => SignUpPage(),
         '/home':(context) => HomePage(),
+        '/instructions':(context) => InstructionsPage(),
       },
     );
   }
