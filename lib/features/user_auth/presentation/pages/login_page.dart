0e30f7a20b19abf9a976bdee1ebec981e5df0af8
mateocapon <mcapon@fi.uba.ar>
2023-11-06 21:55:08 -0300

    agrego login google

--- lib/features/user_auth/presentation/pages/login_page.dart
+++ lib/features/user_auth/presentation/pages/login_page.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:frases_argentinas/features/user_auth/presentation/firebase_auth/firebase_auth_services.dart';
 import 'package:frases_argentinas/features/user_auth/presentation/pages/home_page.dart';
 import 'package:frases_argentinas/features/user_auth/presentation/pages/signup_page.dart';
 import 'package:frases_argentinas/features/user_auth/presentation/widgets/form_container_widget.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../../../../global/common/toast.dart';
 
@@ -14,6 +16,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final FirebaseAuthService _auth = FirebaseAuthService();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   bool _isSigning = false;
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
@@ -68,6 +71,36 @@ class _LoginPageState extends State<LoginPage> {
                   )
               ),
               ),
+              SizedBox(height: 10,),
+              GestureDetector(
+                onTap: () {
+                  _signInWithGoogle();
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(FontAwesomeIcons.google, color: Colors.white,),
+                        SizedBox(width: 5,),
+                        Text(
+                          "Sign in with Google",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
               SizedBox(height: 20,),
               Row(mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -107,6 +140,28 @@ class _LoginPageState extends State<LoginPage> {
       Navigator.pushNamed(context, "/home");
     } else {
       showToast(message: "Incorrect email or password");
+    }
+  }
+
+  void _signInWithGoogle() async {
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+    try {
+      final GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
+      if(googleSignInAccount != null ){
+        final GoogleSignInAuthentication googleSignInAuthentication = await
+        googleSignInAccount.authentication;
+
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken,
+        );
+
+        await _firebaseAuth.signInWithCredential(credential);
+        showToast(message: "User is successfully signed in");
+        Navigator.pushNamed(context, "/home");
+      }
+    }catch(e) {
+      showToast(message: "some error occured $e");
     }
   }
 }
