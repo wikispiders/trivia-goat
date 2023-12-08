b608b28b162fbd68f4a842c309159aa5a30d27a3
Tomas-Bartol <tbartol@fi.uba.ar>
2023-12-08 18:29:08 -0300

    Rediseño de la pantalla de Join. Botón de Volver hacia atrás Validación existe Game ID
    Co-authored-by: milisanchez00 <milisanchez00@users.noreply.github.com>
    Co-authored-by: mateocapon <mateocapon@users.noreply.github.com>

--- lib/widgets/lobby/join_home.dart
+++ lib/widgets/lobby/join_home.dart
@@ -3,7 +3,7 @@ import 'package:frases_argentinas/app_services/app_services.dart';
 import 'package:frases_argentinas/screens/lobby/home_screen.dart';
 
 class JoinHome extends StatefulWidget {
-  const JoinHome({super.key});
+  const JoinHome({Key? key}) : super(key: key);
 
   @override
   State<JoinHome> createState() => _JoinHomeState();
@@ -12,42 +12,73 @@ class JoinHome extends StatefulWidget {
 class _JoinHomeState extends State<JoinHome> {
   TextEditingController _controller = TextEditingController();
 
+  void _joinGame() {
+    final gameId = _controller.text.trim();
+    if (gameId.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please enter a valid Game ID'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else {
+      AppServices().middlewareService.join(gameId);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15.0), // Ajusta el espaciado según sea necesario
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.pink,
-          width: 5.0,
-        ),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-            hintText: 'Inserte un ID de juego',
-            hintStyle: TextStyle(color: Colors.grey, fontSize: 15.0,),
-            labelText: 'Unirse a una partida',
-            labelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0,)),
-            
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.pink, width: 2),
           ),
-          SizedBox(height: 10),
-          ElevatedButton(
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.pink),
             onPressed: () {
-              AppServices().middlewareService.join(_controller.text);
-              
+              AppServices().appStateProvider.setScreen(const HomeScreen());
             },
-            child: Text('UNIRSE'),
           ),
-                    ElevatedButton(
-           onPressed: ()=> AppServices().appStateProvider.setScreen(HomeScreen()),
-            child: Text('VOLVER'),
-          ),
-        ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: 'Insert Game ID',
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15.0,
+                ),
+                labelText: 'Join a game',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: _joinGame,
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              child: const Text('JOIN'),
+            ),
+          ],
+        ),
       ),
     );
   }
