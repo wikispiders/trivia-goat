e09693c5683881e65a5388699c2518ea5d5c6809
milisanchez00 <mvsanchez@fi.uba.ar>
2023-12-08 18:16:38 -0300

    Pantalla Home. Ajuste automatico de la vista al abrirse el teclado.
    Co-authored-by: tomas-bartol <tomas-bartol@users.noreply.github.com>
    Co-authored-by: mateocapon <mateocapon@users.noreply.github.com>

--- lib/screens/lobby/home_screen.dart
+++ lib/screens/lobby/home_screen.dart
@@ -7,47 +7,56 @@ import 'package:provider/provider.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-    @override
+@override
   Widget build(BuildContext context) {
     UsernameProvider userNameProvider = Provider.of<UsernameProvider>(context);
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image.network(
-           'https://opentdb.com/images/logo.png',
-           width: 300,   
-           height: 300,  
-         ),
-          Container(
-            padding: EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.pink,
-                width: 5.0,
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 217, 229),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              'https://opentdb.com/images/logo.png',
+              width: 300,
+              height: 300,
+            ),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.pink,
+                  width: 5.0,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
               ),
-              borderRadius: BorderRadius.circular(10.0),
+              child: TextField(
+                onChanged: userNameProvider.setUsername,
+                decoration: InputDecoration(
+                  hintText: 'Player Game',
+                  hintStyle: TextStyle(color: Colors.grey, fontSize: 15.0),
+                  labelText: 'Insert your name',
+                  labelStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
             ),
-            child: TextField(
-              onChanged: userNameProvider.setUsername,
-              decoration: InputDecoration(hintText: 'Nombre de jugador',
-            hintStyle: TextStyle(color: Colors.grey, fontSize: 15.0,),
-            labelText: 'Ingrese su nombre',
-            labelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0,)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: AppServices().middlewareService.create,
+              child: Text('NUEVA PARTIDA'),
             ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: AppServices().middlewareService.create,
-            child: Text('NUEVA PARTIDA'),
-          ),
-          SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: ()=> AppServices().appStateProvider.setScreen(JoinHome()),
-            child: Text('UNIRSE A UNA PARTIDA'),
-          ),
-          
-        ],
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: ()=> AppServices().appStateProvider.setScreen(JoinHome()),
+              child: Text('UNIRSE A UNA PARTIDA'),
+            ),
+            SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
+          ],
+        ),
       ),
     );
   }
