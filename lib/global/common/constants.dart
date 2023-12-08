69de883502558b49ac20d469df98f79431c50493
Tomas-Bartol <tbartol@fi.uba.ar>
2023-12-08 03:38:13 -0300

    Creo archivo de constantes para el frontend

---
+++ lib/global/common/constants.dart
@@ -0,0 +1,28 @@
+List<String> myCategories = [
+  'General Knowledge',
+  'Books',
+  'Film',
+  'Music',
+  'Musical & Theatres',
+  'TV',
+  'Video Games',
+  'Board Games',
+  'Science & Nature',
+  'Computers',
+  'Mathematics',
+  'Mythology',
+  'Sports',
+  'Geography',
+  'History',
+  'Politics',
+  'Art',
+  'Celebrities',
+  'Animals',
+  'Vehicles',
+  'Comics',
+  'Gadgets',
+  'Anime & Manga',
+  'Cartoon & Animations',
+];
+
+const int categoryOffset = 9;
