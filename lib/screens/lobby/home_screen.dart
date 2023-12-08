b9efda8f27d6453fe15d1d84d2c4bd103e956d71
milisanchez00 <mvsanchez@fi.uba.ar>
2023-12-08 19:03:16 -0300

    Home Page- Botones New Game y Join Name
    Co-authored-by: tomas-bartol <tomas-bartol@users.noreply.github.com>
    Co-authored-by: mateocapon <mateocapon@users.noreply.github.com>

--- lib/screens/lobby/home_screen.dart
+++ lib/screens/lobby/home_screen.dart
@@ -4,12 +4,14 @@ import 'package:frases_argentinas/provider/username_provider.dart';
 import 'package:frases_argentinas/widgets/lobby/join_home.dart';
 import 'package:provider/provider.dart';
 
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
 @override
   Widget build(BuildContext context) {
     UsernameProvider userNameProvider = Provider.of<UsernameProvider>(context);
+    
 
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 255, 217, 229),
@@ -45,16 +47,43 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
+            ElevatedButton.icon(
               onPressed: AppServices().middlewareService.create,
-              child: Text('NUEVA PARTIDA'),
+              icon: Icon(Icons.games),
+              label: Text(
+                  'New Game',
+                  style: TextStyle(fontSize: 25),  // Ajusta el tamaño del texto según tus necesidades
+                ),
+                style: ButtonStyle(
+                elevation: MaterialStateProperty.all<double>(5.0), // Ajusta el valor según tus necesidades
+                shadowColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 136, 175)), // Ajusta el color de la sombra según tus necesidades
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0), // Ajusta el radio de la esquina según tus necesidades
+                ),
+              ),
+              ),
             ),
-            SizedBox(height: 30),
-            ElevatedButton(
+            SizedBox(height: 10),
+            ElevatedButton.icon(
               onPressed: ()=> AppServices().appStateProvider.setScreen(JoinHome()),
-              child: Text('UNIRSE A UNA PARTIDA'),
+              icon: Icon(Icons.start),
+              label: Text(
+                  'Join Game',
+                  style: TextStyle(fontSize: 25),  // Ajusta el tamaño del texto según tus necesidades
+                ),
+              style: ButtonStyle(
+                elevation: MaterialStateProperty.all<double>(5.0), // Ajusta el valor según tus necesidades
+                shadowColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 136, 175)), // Ajusta el color de la sombra según tus necesidades
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0), // Ajusta el radio de la esquina según tus necesidades
+                ),
+              ),
+              ),
             ),
             SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
+
           ],
         ),
       ),
