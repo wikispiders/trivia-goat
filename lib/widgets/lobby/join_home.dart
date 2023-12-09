42d9a8cf80e84d5483d214202ecd2e07e1dbb95d
Tomas-Bartol <tbartol@fi.uba.ar>
2023-12-08 21:09:54 -0300

    Ajustes de pantalla Join Home Mensajes de error
    Co-authored-by: milisanchez00 <milisanchez00@users.noreply.github.com>
    Co-authored-by: mateocapon <mateocapon@users.noreply.github.com>

--- lib/widgets/lobby/join_home.dart
+++ lib/widgets/lobby/join_home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frases_argentinas/app_services/app_services.dart';
 import 'package:frases_argentinas/screens/lobby/home_screen.dart';
-import 'package:frases_argentinas/screens/lobby/constants.dart';
-
 
 class JoinHome extends StatefulWidget {
   const JoinHome({Key? key}) : super(key: key);
@@ -11,9 +9,22 @@ class JoinHome extends StatefulWidget {
   State<JoinHome> createState() => _JoinHomeState();
 }
 
-
 class _JoinHomeState extends State<JoinHome> {
-  TextEditingController _controller = TextEditingController();
+  final TextEditingController _controller = TextEditingController();
+
+  void _joinGame() {
+    final gameId = _controller.text.trim();
+    if (gameId.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please enter a valid Game ID'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else {
+      AppServices().middlewareService.join(gameId);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
