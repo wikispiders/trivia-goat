c561ab0bbc342d28663e22c2ff19660290ca132a
milisanchez00 <mvsanchez@fi.uba.ar>
2023-12-10 15:16:50 -0300

    Pantalla Podio- Transición de las medallas

--- lib/screens/game/end_of_game.dart
+++ lib/screens/game/end_of_game.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:animations/animations.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,22 +26,37 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 10),
-              // Ejemplo con 1 jugador
-              PodiumAvatar(
-                playerName: 'Player 1',
-                rank: 1,
+              
+              FutureBuilder<void>(
+                future: Future.delayed(Duration(seconds: 1)),
+                builder: (context, snapshot) => snapshot.connectionState == ConnectionState.done
+                    ? PodiumAvatar(
+                        playerName: 'Player 1',
+                        rank: 1,
+                      )
+                    : SizedBox.shrink(),
               ),
               SizedBox(height: 10),
-              // Ejemplo con 2 jugadores
-              PodiumAvatar(
-                playerName: 'Player 2',
-                rank: 2,
+              
+              FutureBuilder<void>(
+                future: Future.delayed(Duration(seconds: 3)),
+                builder: (context, snapshot) => snapshot.connectionState == ConnectionState.done
+                    ? PodiumAvatar(
+                        playerName: 'Player 2',
+                        rank: 2,
+                      )
+                    : SizedBox.shrink(),
               ),
               SizedBox(height: 10),
-              // Ejemplo con 3 jugadores
-              PodiumAvatar(
-                playerName: 'Player 3',
-                rank: 3,
+              
+              FutureBuilder<void>(
+                future: Future.delayed(Duration(seconds: 5)),
+                builder: (context, snapshot) => snapshot.connectionState == ConnectionState.done
+                    ? PodiumAvatar(
+                        playerName: 'Player 3',
+                        rank: 3,
+                      )
+                    : SizedBox.shrink(),
               ),
             ],
           ),
@@ -49,7 +66,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class PodiumAvatar extends StatelessWidget {
+class PodiumAvatar extends StatefulWidget {
   final String playerName;
   final int rank;
 
@@ -57,50 +74,83 @@ class PodiumAvatar extends StatelessWidget {
       : super(key: key);
 
   @override
+  _PodiumAvatarState createState() => _PodiumAvatarState();
+}
+
+class _PodiumAvatarState extends State<PodiumAvatar> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    _controller.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      child: Column(
-        children: [
-          if (rank == 1)
-            Image.network(
-              'https://cdn-icons-png.flaticon.com/512/1910/1910476.png', // Medalla de Oro
-              height: 140,
-            ),
-          if (rank == 2)
-            Image.network(
-              'https://cdn-icons-png.flaticon.com/512/1910/1910479.png', // Medalla de Plata
-              height: 120,
-            ),
-          if (rank == 3)
-            Image.network(
-              'https://cdn-icons-png.flaticon.com/512/1910/1910485.png', // Medalla de Bronce
-              height: 110,
-            ),
-          CircleAvatar(
-            radius: 20,
-            backgroundColor: Colors.pink[400],
-            child: CircleAvatar(
+    return FadeTransition(
+      opacity: _animation,
+      child: Container(
+        width: 200,
+        child: Column(
+          children: [
+            if (widget.rank == 1)
+              Image.network(
+                'https://cdn-icons-png.flaticon.com/512/1910/1910476.png', // Medalla de Oro
+                height: 140,
+              ),
+            if (widget.rank == 2)
+              Image.network(
+                'https://cdn-icons-png.flaticon.com/512/1910/1910479.png', // Medalla de Plata
+                height: 120,
+              ),
+            if (widget.rank == 3)
+              Image.network(
+                'https://cdn-icons-png.flaticon.com/512/1910/1910485.png', // Medalla de Bronce
+                height: 110,
+              ),
+            CircleAvatar(
               radius: 20,
-              child: Text(
-                playerName[0], // Muestra la primera letra del nombre
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+              backgroundColor: Colors.pink[400],
+              child: CircleAvatar(
+                radius: 20,
+                child: Text(
+                  widget.playerName[0], // Muestra la primera letra del nombre
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 5),
-          Text(
-            playerName,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+            SizedBox(height: 5),
+            Text(
+              widget.playerName,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
